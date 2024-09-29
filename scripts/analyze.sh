b37665f7ba7d80efc167d6db71a69b7baa9c4223
Théo Devaucoup <theo.devaucoup@pm.me>
2024-09-29 21:54:43 +0200

    add clang-tidy analyze

---
+++ scripts/analyze.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
+SOURCE_DIR=${SCRIPT_DIR}/..
+
+COMMON_DIR=${SOURCE_DIR}/builder
+CXXFLAGS="-DDEBUG_LOG -DORMPP_ENABLE_LOG -I${COMMON_DIR}/third-party -I${COMMON_DIR} -I/usr/include/mysql -I${COMMON_DIR}/third-party/ormpp"
+
+pushd ${SOURCE_DIR}
+declare -a dirs=("builder" "back" "front")
+for dir in "${dirs[@]}"
+do
+    echo Analyze ${dir}...
+    make -C ${dir} -j -k analyze CXXFLAGS="${CXXFLAGS}"
+done
+popd
