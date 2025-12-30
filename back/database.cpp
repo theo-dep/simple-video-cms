5daa3fcba5fa6aaa408d04fbb93c5b82234a5759
Théo Devaucoup <theo.devaucoup@pm.me>
2025-12-30 20:17:53 +0100

    fix compilation with latest gcc 15

--- back/database.cpp
+++ back/database.cpp
@@ -8,6 +8,8 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#pragma GCC diagnostic ignored "-Wdeprecated-literal-operator"
 #include <sqlite_orm.h>
 #pragma GCC diagnostic pop
 
