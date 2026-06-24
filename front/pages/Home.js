d2c5716585a450400b4940348264d45631ea7965
Théo Devaucoup <theo.devaucoup@pm.me>
2026-06-24 17:06:14 +0200

    add video thumbnail component with placeholder

--- front/pages/Home.js
+++ front/pages/Home.js
@@ -1,5 +1,4 @@
 import { html } from 'htm/preact';
-import { api } from '../api.js';
 import { useSearch } from '../hook/useSearch.js';
 import { useTitle } from '../hook/useTitle.js';
 import { user } from '../store/auth.js';
@@ -7,6 +6,7 @@ import { Content } from '../component/Content.js';
 import { UserNav } from '../component/UserNav.js';
 import { SearchInput } from '../component/SearchInput.js';
 import { Footer } from '../component/Footer.js';
+import { VideoThumbnail } from '../component/VideoThumbnail.js';
 
 export default function Home() {
   const allVideos = user.videos.value;
@@ -24,7 +24,7 @@ export default function Home() {
         (v) => html`
           <div class="pure-u-1 pure-u-md-1-3" key=${v.id}>
             <a href=${'/watch-video/' + v.id}>
-              <img src=${api.thumbnailPath(v.id)} class="thumbnail pure-img" alt=${v.title} />
+              <${VideoThumbnail} id=${v.id} title=${v.title} //>
             </a>
           </div>
           <div class="pure-u-1 pure-u-md-2-3" key=${v.id}>
