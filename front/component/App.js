89216b972ca84f2ed431834ab35669572e10ae5a
Théo Devaucoup <theo.devaucoup@pm.me>
2026-06-24 17:17:17 +0200

    wait for refresh auth before loading the app

--- front/component/App.js
+++ front/component/App.js
@@ -24,6 +24,13 @@ export function App() {
 
   useEffect(() => {
     refreshAuth().then(() => {
+      // remove loader after refresh
+      const headRoot = document.getElementById('head-root');
+      headRoot.remove();
+
+      const root = document.getElementById('root');
+      root.remove();
+
       setAuthReady(true);
     });
   }, []);
@@ -41,32 +48,36 @@ export function App() {
     }
   }, [authReady, swReady, user.isLogged.value]);
 
-  return html`
-    <${LocationProvider}>
-      <${Router}>
-        <${lazy(() => import('../pages/Home.js'))} path="/" />
-        <${lazy(() => import('../pages/Login.js'))} path="/login" />
-        <${lazy(() => import('../pages/Logout.js'))} path="/logout" />
-        <${lazy(() => import('../pages/ResetPassword.js'))} path="/reset-password" />
-        <${lazy(() => import('../pages/ResetPassword.js'))} path="/reset-password/:username" />
-        <${lazy(() => import('../pages/UserAccount.js'))} path="/user-account" />
-        <${lazy(() => import('../pages/WatchVideo.js'))} path="/watch-video/:videoId" />
-        <${lazy(() => import('../pages/AdminDashboard.js'))} path="/admin" />
-        <${lazy(() => import('../pages/AdminVideoList.js'))} path="/admin/video-list" />
-        <${lazy(() => import('../pages/AdminNewVideo.js'))} path="/admin/new-video" />
-        <${lazy(() => import('../pages/AdminVideoSettings.js'))} path="/admin/video-settings/:videoId" />
-        <${lazy(() => import('../pages/AdminUserList.js'))} path="/admin/user-list" />
-        <${lazy(() => import('../pages/AdminNewUser.js').then((m) => m.AdminNewUser))} path="/admin/new-user" />
-        <${lazy(() => import('../pages/AdminNewUser.js').then((m) => m.AdminNewAdmin))} path="/admin/new-admin" />
-        <${lazy(() => import('../pages/AdminUserSettings.js').then((m) => m.AdminUserSettings))} path="/admin/user-settings/:userId" />
-        <${lazy(() => import('../pages/AdminUserSettings.js').then((m) => m.AdminAdminSettings))} path="/admin/admin-settings/:adminId" />
-        <${lazy(() => import('../pages/AdminAdminList.js'))} path="/admin/admin-list" />
-        <${lazy(() => import('../pages/AdminGroupList.js'))} path="/admin/group-list" />
-        <${lazy(() => import('../pages/AdminNewGroup.js'))} path="/admin/new-group" />
-        <${lazy(() => import('../pages/AdminGroupSettings.js'))} path="/admin/group-settings/:groupId" />
-        <${lazy(() => import('../pages/Forbidden.js'))} path="/403" />
-        <${lazy(() => import('../pages/NotFound.js'))} default />
+  return (
+    // load components after refresh
+    authReady &&
+    html`
+      <${LocationProvider}>
+        <${Router}>
+          <${lazy(() => import('../pages/Home.js'))} path="/" />
+          <${lazy(() => import('../pages/Login.js'))} path="/login" />
+          <${lazy(() => import('../pages/Logout.js'))} path="/logout" />
+          <${lazy(() => import('../pages/ResetPassword.js'))} path="/reset-password" />
+          <${lazy(() => import('../pages/ResetPassword.js'))} path="/reset-password/:username" />
+          <${lazy(() => import('../pages/UserAccount.js'))} path="/user-account" />
+          <${lazy(() => import('../pages/WatchVideo.js'))} path="/watch-video/:videoId" />
+          <${lazy(() => import('../pages/AdminDashboard.js'))} path="/admin" />
+          <${lazy(() => import('../pages/AdminVideoList.js'))} path="/admin/video-list" />
+          <${lazy(() => import('../pages/AdminNewVideo.js'))} path="/admin/new-video" />
+          <${lazy(() => import('../pages/AdminVideoSettings.js'))} path="/admin/video-settings/:videoId" />
+          <${lazy(() => import('../pages/AdminUserList.js'))} path="/admin/user-list" />
+          <${lazy(() => import('../pages/AdminNewUser.js').then((m) => m.AdminNewUser))} path="/admin/new-user" />
+          <${lazy(() => import('../pages/AdminNewUser.js').then((m) => m.AdminNewAdmin))} path="/admin/new-admin" />
+          <${lazy(() => import('../pages/AdminUserSettings.js').then((m) => m.AdminUserSettings))} path="/admin/user-settings/:userId" />
+          <${lazy(() => import('../pages/AdminUserSettings.js').then((m) => m.AdminAdminSettings))} path="/admin/admin-settings/:adminId" />
+          <${lazy(() => import('../pages/AdminAdminList.js'))} path="/admin/admin-list" />
+          <${lazy(() => import('../pages/AdminGroupList.js'))} path="/admin/group-list" />
+          <${lazy(() => import('../pages/AdminNewGroup.js'))} path="/admin/new-group" />
+          <${lazy(() => import('../pages/AdminGroupSettings.js'))} path="/admin/group-settings/:groupId" />
+          <${lazy(() => import('../pages/Forbidden.js'))} path="/403" />
+          <${lazy(() => import('../pages/NotFound.js'))} default />
+        <//>
       <//>
-    <//>
-  `;
+    `
+  );
 }
