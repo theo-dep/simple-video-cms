a3e29ded8c3b709e944ac1d89eab83e7f7b6f661
Théo Devaucoup <theo.devaucoup@pm.me>
2026-06-28 23:16:06 +0200

    use loader when refreshAuth is called

--- front/pages/Home.js
+++ front/pages/Home.js
@@ -1,23 +1,21 @@
 import { html } from 'htm/preact';
-import { useEffect } from 'preact/hooks';
 import { useSearch } from '../hook/useSearch.js';
 import { useTitle } from '../hook/useTitle.js';
-import { refreshAuth, user } from '../store/auth.js';
+import { useLoader } from '../hook/useLoader.js';
+import { refreshAuth, user, refreshRequested } from '../store/auth.js';
 import { Content } from '../component/Content.js';
 import { UserNav } from '../component/UserNav.js';
 import { SearchInput } from '../component/SearchInput.js';
 import { Footer } from '../component/Footer.js';
 import { VideoThumbnail } from '../component/VideoThumbnail.js';
+import { Loader } from '../component/Loader.js';
 
 export default function Home() {
   const allVideos = user.videos.value;
   const { results, search } = useSearch(allVideos, ['title']);
+  const { isLoading } = useLoader(refreshAuth, !refreshRequested.value);
 
   useTitle('Home');
-
-  useEffect(() => {
-    refreshAuth();
-  }, []);
 
   return html`
     <${UserNav}>
@@ -25,8 +23,10 @@ export default function Home() {
     <//>
 
     <${Content} class="pure-g">
-      ${results.map(
-        (v) => html`
+      ${isLoading
+        ? html`<${Loader} />`
+        : results.map(
+            (v) => html`
           <div class="pure-u-1 pure-u-md-1-3" key=${v.id}>
             <a href=${'/watch-video/' + v.id}>
               <${VideoThumbnail} id=${v.id} title=${v.title} //>
@@ -37,7 +37,7 @@ export default function Home() {
           </div>
           <hr class="pure-u-1" />
         `
-      )}
+          )}
     <//>
 
     <${Footer} />
