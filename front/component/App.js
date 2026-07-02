47e12259840b4302995b9931e1ff4c5b68d6a6cd
Théo Devaucoup <theo.devaucoup@pm.me>
2026-07-02 14:03:54 +0200

    redirect to 403 in page level

--- front/component/App.js
+++ front/component/App.js
@@ -5,6 +5,7 @@ import { LocationProvider, Router, lazy } from 'preact-iso';
 import { refreshed } from '../store/auth.js';
 import { swReady } from '../store/sw.js';
 import { ConfirmDialog } from './ConfirmDialog.js';
+import { adminLazy, adminLazyNamed } from '../utils/lazy.js';
 
 export function App() {
   const isLoading = !refreshed.value || !swReady.value;
@@ -40,21 +41,21 @@ export function App() {
               <${lazy(() => import('../pages/ResetPassword.js'))} path="/reset-password/:username" />
               <${lazy(() => import('../pages/UserAccount.js'))} path="/user-account" />
               <${lazy(() => import('../pages/WatchVideo.js'))} path="/watch-video/:videoId" />
-              <${lazy(() => import('../pages/AdminDashboard.js'))} path="/admin" />
-              <${lazy(() => import('../pages/AdminVideoList.js'))} path="/admin/video-list" />
-              <${lazy(() => import('../pages/AdminNewVideo.js'))} path="/admin/new-video" />
-              <${lazy(() => import('../pages/AdminVideoSettings.js'))} path="/admin/video-settings/:videoId" />
-              <${lazy(() => import('../pages/AdminUserList.js'))} path="/admin/user-list" />
-              <${lazy(() => import('../pages/AdminNewUser.js').then((m) => m.AdminNewUser))} path="/admin/new-user" />
-              <${lazy(() => import('../pages/AdminNewUser.js').then((m) => m.AdminNewAdmin))} path="/admin/new-admin" />
-              <${lazy(() => import('../pages/AdminUserSettings.js').then((m) => m.AdminUserSettings))} path="/admin/user-settings/:userId" />
-              <${lazy(() => import('../pages/AdminUserSettings.js').then((m) => m.AdminAdminSettings))} path="/admin/admin-settings/:adminId" />
-              <${lazy(() => import('../pages/AdminAdminList.js'))} path="/admin/admin-list" />
-              <${lazy(() => import('../pages/AdminGroupList.js'))} path="/admin/group-list" />
-              <${lazy(() => import('../pages/AdminNewGroup.js'))} path="/admin/new-group" />
-              <${lazy(() => import('../pages/AdminGroupSettings.js'))} path="/admin/group-settings/:groupId" />
               <${lazy(() => import('../pages/Forbidden.js'))} path="/403" />
               <${lazy(() => import('../pages/NotFound.js'))} default />
+              <${adminLazy(() => import('../pages/AdminDashboard.js'))} path="/admin" />
+              <${adminLazy(() => import('../pages/AdminVideoList.js'))} path="/admin/video-list" />
+              <${adminLazy(() => import('../pages/AdminNewVideo.js'))} path="/admin/new-video" />
+              <${adminLazy(() => import('../pages/AdminVideoSettings.js'))} path="/admin/video-settings/:videoId" />
+              <${adminLazy(() => import('../pages/AdminUserList.js'))} path="/admin/user-list" />
+              <${adminLazyNamed(() => import('../pages/AdminNewUser.js'), 'AdminNewUser')} path="/admin/new-user" />
+              <${adminLazyNamed(() => import('../pages/AdminNewUser.js'), 'AdminNewAdmin')} path="/admin/new-admin" />
+              <${adminLazyNamed(() => import('../pages/AdminUserSettings.js'), 'AdminUserSettings')} path="/admin/user-settings/:userId" />
+              <${adminLazyNamed(() => import('../pages/AdminUserSettings.js'), 'AdminAdminSettings')} path="/admin/admin-settings/:adminId" />
+              <${adminLazy(() => import('../pages/AdminAdminList.js'))} path="/admin/admin-list" />
+              <${adminLazy(() => import('../pages/AdminGroupList.js'))} path="/admin/group-list" />
+              <${adminLazy(() => import('../pages/AdminNewGroup.js'))} path="/admin/new-group" />
+              <${adminLazy(() => import('../pages/AdminGroupSettings.js'))} path="/admin/group-settings/:groupId" />
             <//>
           <//>
         <//>
