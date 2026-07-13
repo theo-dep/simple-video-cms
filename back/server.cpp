8451017e746230c8877a19ac519abd7310649ef6
Théo Devaucoup <theo.devaucoup@pm.me>
2026-07-13 22:36:30 +0200

    fix old watch video route

--- back/server.cpp
+++ back/server.cpp
@@ -31,7 +31,7 @@ namespace server
     void set_exception_handler(httplib::Server& server);
 
     // Routes
-    void watch_video(const httplib::Request& req, httplib::Response& res, const std::filesystem::path& bundle_dir, const Database& db);
+    void video(const httplib::Request& req, httplib::Response& res, const std::filesystem::path& bundle_dir, const Database& db);
     void static_file(const httplib::Request& req, httplib::Response& res, const std::filesystem::path& bundle_dir);
     void index(const httplib::Request& req, httplib::Response& res, const std::filesystem::path& bundle_dir);
     void manifest(const httplib::Request& req, httplib::Response& res, const std::filesystem::path& bundle_dir);
@@ -130,7 +130,7 @@ int server::start()
     set_exception_handler(server);
 
     server
-        .Get("/watch-video/:id", sc::serve(watch_video, std::cref(bundle_dir), std::cref(db)))
+        .Get("/video/:id", sc::serve(video, std::cref(bundle_dir), std::cref(db)))
         .Get(R"(.*\/manifest\.json$)", sc::serve(manifest, std::cref(bundle_dir)))
         .Get(R"((?!\/api\/).*\.[^/]+$)", sc::serve(static_file, std::cref(bundle_dir)))
         .Get(R"((?!\/api\/).*)", sc::serve(index, std::cref(bundle_dir)))
@@ -306,13 +306,13 @@ namespace server
     }
 }
 
-inline void server::watch_video(const httplib::Request& req, httplib::Response& res, const std::filesystem::path& bundle_dir, const Database& db)
+inline void server::video(const httplib::Request& req, httplib::Response& res, const std::filesystem::path& bundle_dir, const Database& db)
 {
     const std::string video_id{ req.path_params.at("id") };
     const std::string title{ db.video_title(su::string_to_int(video_id)) };
     const std::string description{ "Watch " + title + " video" };
     const std::string thumbnail_url{ "/api/thumbnail/" + video_id };
-    const std::string website_url{ "/watch-video/" + video_id };
+    const std::string website_url{ "/video/" + video_id };
     serve_index(req, res, bundle_dir,
                 { .title = title,
                   .description = description,
@@ -696,8 +696,8 @@ namespace server
         return db.has_video_right(video_id);
     }
 
-    // block video if not in watch-video page
-    inline bool request_from_watch_video(const httplib::Request& req, int video_id)
+    // block video if not in video page
+    inline bool request_from_video(const httplib::Request& req, int video_id)
     {
         const std::string referrer{ req.get_header_value("Referer") };
         return referrer.ends_with("/video/" + su::int_to_string(video_id));
@@ -712,7 +712,7 @@ inline void server::video_playlist(const httplib::Request& req, httplib::Respons
     }
 
     const int video_id{ su::string_to_int(req.path_params.at("video_id")) };
-    if (!request_from_watch_video(req, video_id)) {
+    if (!request_from_video(req, video_id)) {
         res.status = httplib::StatusCode::Forbidden_403;
         return;
     }
@@ -746,7 +746,7 @@ inline void server::video_segment(const httplib::Request& req, httplib::Response
     }
 
     const int video_id{ su::string_to_int(req.path_params.at("video_id")) };
-    if (!request_from_watch_video(req, video_id)) {
+    if (!request_from_video(req, video_id)) {
         res.status = httplib::StatusCode::Forbidden_403;
         return;
     }
@@ -776,7 +776,7 @@ inline void server::add_video_session(const httplib::Request& req, httplib::Resp
     }
 
     const int video_id{ su::string_to_int(req.path_params.at("video_id")) };
-    if (!request_from_watch_video(req, video_id)) {
+    if (!request_from_video(req, video_id)) {
         res.status = httplib::StatusCode::Forbidden_403;
         return;
     }
@@ -795,7 +795,7 @@ inline void server::start_video_session(const httplib::Request& req, httplib::Re
     }
 
     const int video_id{ su::string_to_int(req.path_params.at("video_id")) };
-    if (!request_from_watch_video(req, video_id)) {
+    if (!request_from_video(req, video_id)) {
         res.status = httplib::StatusCode::Forbidden_403;
         return;
     }
@@ -814,7 +814,7 @@ inline void server::reset_video_session(const httplib::Request& req, httplib::Re
     }
 
     const int video_id{ su::string_to_int(req.path_params.at("video_id")) };
-    if (!request_from_watch_video(req, video_id)) {
+    if (!request_from_video(req, video_id)) {
         res.status = httplib::StatusCode::Forbidden_403;
         return;
     }
